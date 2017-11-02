b9487a40ad824268fa52bf8f89a15b11170b4471
Michael <liukaiyi54@gmail.com>
2017-11-02 18:52:46 +0800

    first commit

---
+++ src/main/java/com/example/demo/Hello/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Hello;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Michael on 02/11/2017.
+ */
+public interface UserRepository extends CrudRepository<User, Long>{
+
+}
